5257ad00b22e90444aa40a2082124ece701b639a
cancai <710180334@qq.com>
2016-06-13 20:11:16 +0800

    change something

--- main.cpp
+++ main.cpp
@@ -148,13 +148,69 @@ void Exit()
 	printf("\nPlease type enter key to exit. \n");
 	getchar();
 }
+// 参数说明:  
+// in， 源字符串  
+// out, 存放最后结果的字符串  
+// outlen，out最大的大小  
+// src，要替换的字符串  
+// dst，替换成什么字符串  
+char *strrpl(char *in, char *out, int outlen, const char *src, char *dst)
+{
+	char *p = in;
+	unsigned int  len = outlen - 1;
+
+	// 这几段检查参数合法性  
+	if ((NULL == src) || (NULL == dst) || (NULL == in) || (NULL == out))
+	{
+		return NULL;
+	}
+	if ((strcmp(in, "") == 0) || (strcmp(src, "") == 0))
+	{
+		return NULL;
+	}
+	if (outlen <= 0)
+	{
+		return NULL;
+	}
+
+	while ((*p != '\0') && (len > 0))
+	{
+		if (strncmp(p, src, strlen(src)) != 0)
+		{
+			int n = strlen(out);
+
+			out[n] = *p;
+			out[n + 1] = '\0';
+
+			p++;
+			len--;
+		}
+		else
+		{
+			strcat_s(out, outlen, dst);
+			p += strlen(src);
+			len -= strlen(dst);
+		}
+	}
+	return out;
+}
+char *replacestr(char *strbuf, char sc, char dc)
+{
+	char *p = strbuf;
+	while (*strbuf != '\0')
+	{
+		if (*strbuf == sc)
+			*strbuf = dc;
+		strbuf++;
+	}
+	return p;
+}
 int main(int argc, const char ** argv)
 {
 	const char* xml_file;
 	const char* out_put_dir;
-	//try
-	//{
-
+	try
+	{
 		// help
 		if (argc == 2 && strcmp(argv[1], "--help") == 0)
 		{
@@ -232,7 +288,8 @@ int main(int argc, const char ** argv)
 			entry = entry->NextSiblingElement("entry"))
 		{
 			b = true;
-			const char* cpathStr = entry->Attribute("path");
+			char* p = (char*)entry->Attribute("path");
+			const char* cpathStr = replacestr(p, '\\', '/');
 			XMLElement* status = entry->FirstChildElement("wc-status");
 			XMLElement* revision = status->FirstChildElement("commit");
 			if (strchr(cpathStr, '.'))
@@ -241,7 +298,7 @@ int main(int argc, const char ** argv)
 				{
 					const char* crevisionStr = revision->Attribute("revision");
 					// floder or only main swf file
-					std::vector<std::string> v = split(cpathStr, "\\");
+					std::vector<std::string> v = split(cpathStr, "/");
 					if (v.size() > 1)
 					{
 						string group_xml_name = v[0];
@@ -357,11 +414,11 @@ int main(int argc, const char ** argv)
 			printf("all finished.\n\n");
 			Exit();
 		}
-	//}
-	//catch (exception e)
-	/*{
+	}
+	catch (exception e)
+	{
 		printf("Exception: %s",e.what());
 		Exit();
-	}*/
+	}
 	return 0;
 }
